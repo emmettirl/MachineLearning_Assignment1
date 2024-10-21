0a20e6470018301fb9faa9a4aa639a841fb55a2f
Emmett <emmett.fitzharris@mycit.ie>
2024-10-21 18:24:49 +0100

    task 4 complete

--- src/main().py
+++ src/main().py
@@ -22,8 +22,8 @@ def main():
     # Task 1
     training_df, test_df = task1()
     train_word_list, test_word_list = task2(training_df, test_df, minimumWordLength, minimumWordOccurrence)
-    task3(training_df, test_df, train_word_list, test_word_list, minimumWordLength, minimumWordOccurrence)
-    task4()
+    train_word_counts_pos, train_word_counts_neg, test_word_counts_pos, test_word_counts_neg = task3(training_df, test_df, train_word_list, test_word_list, minimumWordLength, minimumWordOccurrence)
+    task4(training_df, test_df, train_word_counts_pos, train_word_counts_neg, test_word_counts_pos, test_word_counts_neg)
     # task5()
     # task6()
 
@@ -91,14 +91,73 @@ def task3(training_df, test_df, train_word_list, test_word_list, minimum_word_le
     print(test_word_counts_neg.sort_values(by='review_count', ascending=False))
     print_divider("-")
 
-def task4():
+    return train_word_counts_pos, train_word_counts_neg, test_word_counts_pos, test_word_counts_neg
+
+def task4(training_df, test_df, train_word_counts_pos, train_word_counts_neg, test_word_counts_pos, test_word_counts_neg):
     print_header("#", "Task 4")
+
+    # To calculate conditional probability, I need to find the probability a word is in a review,
+    # and the probability a review is positive,
+    # then divide the former by the latter.
+
+    # calculate priors
+    training_positive_probability = calculatePriors(training_df, 'positive')
+    training_negative_probability = calculatePriors(training_df, 'negative')
+    test_positive_probability = calculatePriors(test_df, 'positive')
+    test_negative_probability = calculatePriors(test_df, 'negative')
+
+    print("Priors")
+    print(f"Training DataProbability. Positive: {training_positive_probability:.2f}, Negative: {training_negative_probability:.2f}")
+    print(f"Test Data Probability. Positive: {test_positive_probability:.2f}, Negative: {test_negative_probability:.2f}")
+
+    print_divider("-")
+
+    # calculate conditional probability
+    train_word_counts_pos = calculateConditionalProbability(train_word_counts_pos, training_df.shape[0], training_positive_probability)
+    train_word_counts_neg = calculateConditionalProbability(train_word_counts_neg, training_df.shape[0], training_negative_probability)
+    test_word_counts_pos = calculateConditionalProbability(test_word_counts_pos, test_df.shape[0], test_positive_probability)
+    test_word_counts_neg = calculateConditionalProbability(test_word_counts_neg, test_df.shape[0], test_negative_probability)
+
+    print(f" Number of training Reviews: {training_df.shape[0]}")
+    print(f" Number of test Reviews: {test_df.shape[0]}")
+    print_divider("-")
+
+    print("Training Data")
+    print("\nPositive")
+    print (train_word_counts_pos)
+    print("\nNegative")
+    print (train_word_counts_neg)
+    print_divider("-")
+
+    print("Test Data")
+    print("\nPositive")
+    print (test_word_counts_pos)
+    print("\nNegative")
+    print (test_word_counts_neg)
+
+
+
+
+
+
+def calculatePriors(df, sentiment):
+    return df[df['Sentiment'] == sentiment].shape[0] / df.shape[0]
+
+def calculateConditionalProbability(word_counts_df, review_count, sentiment_probability):
+    # Calculate the probability of each word being in a review
+    word_counts_df['probability'] = word_counts_df['review_count'] / review_count
+
+    # Calculate the conditional probability of each word being in a review, given the review is positive
+    word_counts_df['conditional_probability'] = word_counts_df['probability'] / sentiment_probability
+    return word_counts_df
+
 
 def task5():
     print_header("#", "Task 5")
 
 def task6():
     print_header("#", "Task 6")
+
 
 def word_in_review_occurrences(df, word_list, minimum_word_length, minimum_word_occurrence, cache_name, sentiment):
     words_cache_file = source_filename + "-" + cache_name + "-" + str(minimum_word_length) + "-" + str(
@@ -176,6 +235,7 @@ def read_data():
     # it returns the text column and the sentiment column as separate arrays
 
 
+# data is being split based on the value in the 'Split' column, rather than a random split factor
 def split_data(df):
     training_data = df[df['Split'] == 'train']['Review']
     training_labels = df[df['Split'] == 'train']['Sentiment']
