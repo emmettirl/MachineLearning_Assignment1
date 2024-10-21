361c0f58418e22ad8d8bf42fc2c1e48eee0f78c4
Emmett <emmett.fitzharris@mycit.ie>
2024-10-21 12:47:45 +0100

    task 2 complete

--- src/main().py
+++ src/main().py
@@ -7,20 +7,32 @@ import pickle
 
 import time
 
+from pandas import value_counts
 
 source_filename = 'movie_reviews.xlsx'
 cache_filename = source_filename + ".pkl"
-split_factor = 0.8
 
 def main():
     start_time = time.time() # start measuring time
 
+# Task 1
     trainingData, trainingLabels, testData, testLabels = task1()
+
+# bundling into a test and training data frame
+    trainingDf = pd.DataFrame({'Review': trainingData, 'Sentiment': trainingLabels})
+    testDf = pd.DataFrame({'Review': testData, 'Sentiment': testLabels})
+
+# Task 2
+    minimumWordLength, minimumWordOccurence = 3, 15
+    trainWordList, testWordList = task2(trainingDf, testDf, minimumWordLength, minimumWordOccurence)
+
+# Task 3
+
+    task3()
+
 
     print(f"Execution time: {time.time() - start_time:.2f} seconds")     # calculate elapsed
 
-
-    task2(trainingData, trainingLabels, testData, testLabels)
 
 def task1():
     printHeader("#", "Task 1")
@@ -37,8 +49,41 @@ def task1():
 
     return trainingData, trainingLabels, testData, testLabels
 
-def task2(trainingData, trainingLabels, testData, testLabels):
+def task2(trainingDf, testDf, minimumWordLength, minimumWordOccurence):
     printHeader("#", "Task 2")
+
+    train_wordList = extract_features(trainingDf, minimumWordLength, minimumWordOccurence)
+    print(f"Number of unique words in training data, longer than {minimumWordLength} characters, which occure more than {minimumWordOccurence} times: " + str(len(train_wordList)))
+
+    test_wordList = extract_features(testDf, minimumWordLength, minimumWordOccurence)
+    print(f"Number of unique words in test data, longer than {minimumWordLength} characters, which occure more than {minimumWordOccurence} times: " + str(len(test_wordList)))
+
+    return train_wordList, test_wordList
+
+def task3():
+    printHeader("#", "Task 3")
+
+def extract_features(data, minimumWordLength, minimumWordOccurence):
+    # replace all dashes with spaces
+    data["Review"] = data["Review"].replace("-", ' ', regex=True)
+    # remove all non-alphanumeric characters
+    pattern = r'[^\w\s]'
+    data["Review"] = data["Review"].replace(pattern, '', regex=True)
+    # convert to lowercase
+    data["Review"] = data["Review"].str.lower()
+    # split the words
+    data["Review"] = data["Review"].str.split()
+    # explode the words into separate rows
+    word_data = pd.DataFrame({"words": data["Review"].explode()})
+
+    word_data = word_data[word_data["words"].str.len() >= minimumWordLength]
+    word_vc = word_data['words'].value_counts().reset_index()
+    word_vc.columns = ['Word', 'Count']
+    word_vc = word_vc[word_vc["Count"] >= minimumWordOccurence]
+
+    wordlist = word_vc['Word'].tolist()
+    return wordlist
+
 
 # Read data from the Excel file.
 # The file is large, and this process is slow
